b7154b64cd960f4c460b86e776844e4f7ba2364a
krefature <77618655+krefature@users.noreply.github.com>
2025-02-24 16:49:57 +0600

    1 - 4

--- main.cpp
+++ main.cpp
@@ -1,25 +1,72 @@
 #include <iostream>
+using namespace std;
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+struct box {
+    public:
+    box(int length, int width, int height, double weight, int value)
+        : length(length),
+          width(width),
+          height(height),
+          weight(weight),
+          value(value) {
+    }
+    int getLength() const {
+        return length;
+    }
+    int getWidth() const {
+        return width;
+    }
+    int getHeight() const {
+        return height;
+    }
+    double getWeight() const {
+        return weight;
+    }
+    int getValue() const {
+        return value;
+    }
+    static int boxesSumm(const vector<box>& boxes) {
+        int sum = 0;
+        for (box b : boxes) {
+            sum += b.getValue();
+        }
+        return sum;
+    }
+    static bool boxesIsLegit(const vector<box>& boxes, int maxL) {
+        int sum = 0;
+        for (box b : boxes) {
+            sum += b.getLength() + b.getWidth() + b.getHeight();
+        }
+        return sum <= maxL;
+    }
+    static double boxesMaxWeight(const vector<box>& boxes, int maxV) {
+        int maxWeight = 0;
+        for (box b : boxes) {
+            if (b.getWeight() > maxWeight && b.getLength()*b.getHeight()*b.getWidth() <= maxV) {
+                maxWeight = b.getWeight();
+            }
+        }
+        return maxWeight;
     }
 
+
+    private:
+        int length;
+        int width;
+        int height;
+        double weight;
+        int value;
+};
+
+int main() {
+    box b1{6,5,6,4,5};
+    box b2{5,4,5,1,5};
+    box b3{2,3,2,10,5};
+    box b4{1,1,1,5,5};
+    vector<box> boxes = {b1,b2,b3,b4};
+    cout << boolalpha;
+    cout << "Суммарная стоимость = " << box::boxesSumm(boxes) << endl;
+    cout << "Меньше? - " << box::boxesIsLegit(boxes,12000) << endl;
+    cout << "Максимальный вес = " << box::boxesMaxWeight(boxes,12) << endl;
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
